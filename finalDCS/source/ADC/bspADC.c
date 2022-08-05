78dddc6b076ce8792711b1ac169db465eb3ab215
cokfi <74899649+cokfi@users.noreply.github.com>
2022-08-05 15:57:53 +0300

    hal adc bsp

--- finalDCS/source/ADC/bspADC.c
+++ finalDCS/source/ADC/bspADC.c
@@ -2,10 +2,22 @@
  * bspADC.c
  *
  *  Created on: 3 Aug 2022
- *      Author: rontal
+ *      Author: rontal (aka tajoro)
  */
 
 #include  "../../header/ADC/bspADC.h"
+
+
+
+void adcReset(){
+    ADC10CTL0 = 0x00; 
+    ADC10CTL1 = 0x00;
+
+}
+
+void adcSetInputChannel(InputChannel ic){
+ADC10CTL1 = ic;                       // input A1-A3
+}
 
 
 
