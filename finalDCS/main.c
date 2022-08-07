b9889b92bc9f045bb0673ba5de9a8d0d6223bd47
Tajoro <ron.joe.tal@gmail.com>
2022-08-07 21:32:36 +0300

    ADC10 Test  (Incomplete)
    Test is on main.c. Connect P3 to LEDs and joystick to ports related to A0, A1 (see datasheet).
    
    If working, v_x and v_y should be displayed on LEDs

--- finalDCS/main.c
+++ finalDCS/main.c
@@ -7,42 +7,63 @@
  */
 static unsigned int motorSequence = 0x10;
 
+static int v_x;
+static int v_y;
+static int current_adc_channel = 0;
+
+void configureADC()
+{
+    ADC10CTL0 = 0;
+    ADC10CTL0 |= ADC10ON; // Turn Module On
+    ADC10CTL1 |= CONSEQ_3; // Repeat Sequence of Channels Mode
+    ADC10CTL1 |= SHS_0; // SH
+    ADC10CTL1 |= INCH_1; // Highest Channel in the sequence is A1
+    ADC10CTL1 |= ADC10DF; // 2's Complement format
+//ADC10CTL1 &= ~ADC10DF;// Binary format
+}
+
+void startADC()
+{
+    ADC10CTL0 |= ADC10IE;
+    ADC10CTL0 |= ENC; // Enable Conversions (Must be reset before changing configuration)
+    ADC10CTL0 |= ADC10SC; // Start Conversion
+}
+
 int main(void)
 {
 
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-    P1SEL = 0;
-    P1DIR = 0xF0;
+    // Connect P3 to LEDs to see values
+    P3SEL = 0;
+    P3DIR = 0xFF;
 
-    TACTL = 0;
-    TACTL |= ID_3;
-    TACCR0 = 82;
-
-    TACTL |= MC_1 + TAIE;
-
-    int i;
-    for (i = 0; i < 30; i++)
+    configureADC();
+    startADC();
+    while (1)
     {
 
-        if (motorSequence == 0x08)
-        {
-            motorSequence = 0x80;
-        }
-        else
-        {
-            motorSequence = motorSequence >> 1;
-        }
+        __bis_SR_register_on_exit(CPUOFF + GIE);
+        int v_x_disp, v_y_disp;
+        v_x_disp = v_x & 0x0F; // Keep right 4 bits
+        v_y_disp = v_y << 4 & 0xF0; // Keep left 4 bits
 
-        P1OUT = motorSequence;
-        _BIS_SR(LPM0_bits);
+        P3OUT = v_x_disp | v_y_disp;
     }
     return 0;
 }
 
-#pragma vector = TIMER0_A0_VECTOR
-__interrupt void TIMER0_A0_ISR(void)
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
 {
-    TACTL &= ~TAIFG;
-    LPM0_EXIT;
+    if (current_adc_channel == 0)
+    {
+        v_x = ADC10MEM;
+    }
+    else
+    {
+        v_y = ADC10MEM;
+    }
+    current_adc_channel ^= current_adc_channel;
+    __bic_SR_register_on_exit(CPUOFF); // Enable CPU so the main while loop continues
 }
