fc0fa3cac0308cb85115209a5afd917e4e837ba3
cokfi <74899649+cokfi@users.noreply.github.com>
2022-08-08 21:28:08 +0300

    uart preparation

--- finalDCS/main.c
+++ finalDCS/main.c
@@ -10,6 +10,7 @@ static unsigned int motorSequence = 0x10;
 static unsigned int v_x = 0x00;
 static int v_y;
 static int current_adc_channel = 0;
+static int counter =0;
 
 void configureADC()
 {
@@ -19,12 +20,10 @@ void configureADC()
     ADC10CTL1 |= SHS_0; // SH
     ADC10CTL1 |= INCH_1; // Highest Channel in the sequence is A1
     ADC10CTL1 |= ADC10DF + ADC10DIV_4; // 2's Complement format
-//ADC10CTL1 &= ~ADC10DF;// Binary format
 }
 
 void startADC()
 {
-    //ADC10CTL0 |= ADC10IE;
     ADC10CTL0 |= ENC; // Enable Conversions (Must be reset before changing configuration)
     ADC10CTL0 |= ADC10SC; // Start Conversion
 }
@@ -40,18 +39,10 @@ int main(void)
 
     configureADC();
     startADC();
-    int v_x_disp, v_y_disp;
     while (1)
     {
-        //ADC10CTL0 &= ~ADC10IE;
         P2OUT = (unsigned int)ADC10MEM>>8;
-        //ADC10CTL0 |= ADC10IE;
-        //__bis_SR_register(CPUOFF + GIE);
-        //v_x_disp = v_x & 0x0F; // Keep right 4 bits
-        //v_y_disp = v_y << 4 & 0xF0; // Keep left 4 bits
-
     }
-    return 0;
 }
 
 #pragma vector=ADC10_VECTOR
@@ -69,4 +60,46 @@ __interrupt void ADC10_ISR(void)
     }
     current_adc_channel ^= current_adc_channel;
     __bic_SR_register_on_exit(CPUOFF); // Enable CPU so the main while loop continues
+}
+
+
+//-------------------------------------------------------------
+//           interrupt vector uartTx
+//-------------------------------------------------------------
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCIAB0TX_VECTOR
+__interrupt void USCI0TX_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCIAB0TX_VECTOR))) USCI0TX_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+
+            UCA0TXBUF = ADC10MEM;                 // TX next sample
+            if (counter >=1){
+                IE2 &= ~UCA0TXIE;                       // Disable USCI_A0 TX interrupt
+                counter = 0;
+            }
+            counter++;
+
+}
+
+//-------------------------------------------------------------
+//           interrupt vector uartRx
+//-------------------------------------------------------------
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCIAB0RX_VECTOR
+__interrupt void USCI0RX_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCIAB0RX_VECTOR))) USCI0RX_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    if (UCA0RXBUF == '1')                     // '1' received?
+    {
+        IE2 |= UCA0TXIE;                        // Enable USCI_A0 TX interrupt
+        UCA0TXBUF = ADC10MEM;
+
 }
