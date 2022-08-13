f0fddc9bbf4ef1099c836f94e976fec03cd4fa32
cokfi <74899649+cokfi@users.noreply.github.com>
2022-08-13 23:53:21 +0300

    end of day with python changes

--- finalDCS/Python/main.py
+++ finalDCS/Python/main.py
@@ -3,255 +3,267 @@ import serial as ser
 import time
 import PySimpleGUI as sg  # Part 1 - The import
 
-#in this example we send a character, and receive a string in response only if the start character was 'u'
-BUTTON_SIZE = (20,1)
-BUTTON_COLOR_UNPRESSED = ('white','black')
-BUTTON_COLOR_CHOSEN = ('black','white')
-BUTTON_COLOR_PRESSED = ('black','blue')
-CANVAS_RESOLUTION = (480,480)
-POINT_SIZE = (4,4)
-MODE_TEXT = ["Write","Erase","Neutral"]
+# in this example we send a character, and receive a string in response only if the start character was 'u'
+BUTTON_SIZE = (20, 1)
+BUTTON_COLOR_UNPRESSED = ('white', 'black')
+BUTTON_COLOR_CHOSEN = ('black', 'white')
+BUTTON_COLOR_PRESSED = ('black', 'blue')
+CANVAS_RESOLUTION = (480, 480)
+POINT_SIZE = (4, 4)
+MODE_TEXT = ["Write", "Erase", "Neutral"]
 
-UP_THRESHOLD = 120 # Need to check real values
-DOWN_THRESHOLD = -120 # Need to check real values
+UP_THRESHOLD = 120  # Need to check real values
+DOWN_THRESHOLD = -120  # Need to check real values
 
 # COMMUNICATION MESSAGES #
-EXIT_MESSAGE ='b'
-ERROR_MESSAGE ='e'
-ACKNOWLEDGE_MESSAGE ='a'
-BUTTON_PRESSED_MESSAGE ='p'
-UP_MESSAGE ='u'
-DOWN_MESSAGE ='d'
+EXIT_MESSAGE = 'b'
+ERROR_MESSAGE = 'e'
+ACKNOWLEDGE_MESSAGE = 'a'
+BUTTON_PRESSED_MESSAGE = 'p'
+UP_MESSAGE = 'u'
+DOWN_MESSAGE = 'd'
 
 
 class MainMenu:
-	def __init__(self):
-		if __name__=='__main__':
-			button_1 = sg.Button("1. Motor Control",key=1,size=BUTTON_SIZE,button_color=BUTTON_COLOR_UNPRESSED)
-			button_2 = sg.Button("2. Painter",key=2,size=BUTTON_SIZE,button_color=BUTTON_COLOR_UNPRESSED)
-			button_3 = sg.Button("3. Stepper Calibration",key=3,size=BUTTON_SIZE,button_color=BUTTON_COLOR_UNPRESSED)
-			button_4 = sg.Button("4. Script Mode",key=4,size=BUTTON_SIZE,button_color=BUTTON_COLOR_UNPRESSED)
-			layout = [[button_1],[button_2],[button_3],[button_4]]
-			self.window = sg.Window('DCS Final Project Main Menu',layout,resizable=True,return_keyboard_events=True)
+    def __init__(self):
+        if __name__ == '__main__':
+            button_1 = sg.Button("1. Motor Control", key=1, size=BUTTON_SIZE, button_color=BUTTON_COLOR_UNPRESSED)
+            button_2 = sg.Button("2. Painter", key=2, size=BUTTON_SIZE, button_color=BUTTON_COLOR_UNPRESSED)
+            button_3 = sg.Button("3. Stepper Calibration", key=3, size=BUTTON_SIZE, button_color=BUTTON_COLOR_UNPRESSED)
+            button_4 = sg.Button("4. Script Mode", key=4, size=BUTTON_SIZE, button_color=BUTTON_COLOR_UNPRESSED)
+            layout = [[button_1], [button_2], [button_3], [button_4]]
+            self.window = sg.Window('DCS Final Project Main Menu', layout, resizable=True, return_keyboard_events=True, finalize=True)
+
 
 class PainterMode:
-	WRITE = 0
-	ERASE = 1
-	NEUTRAL = 2
+    WRITE = 0
+    ERASE = 1
+    NEUTRAL = 2
+
 
 class PaintCursor:
-	x = CANVAS_RESOLUTION[0]//2
-	y = CANVAS_RESOLUTION[1]//2
+    x = CANVAS_RESOLUTION[0] // 2
+    y = CANVAS_RESOLUTION[1] // 2
+
 
 class Painter:
-	def __init__(self, UART):
-		self.UART = UART
-		self.canvas = sg.Graph(CANVAS_RESOLUTION,(0,0),CANVAS_RESOLUTION,key='CANVAS',background_color='white')
-		self.cursor = PaintCursor()
-		self.mode = PainterMode.NEUTRAL
-		layout = [[self.canvas],[sg.Text(MODE_TEXT[self.mode],key='Mode')]]
-		self.window = sg.Window("Painter",layout,return_keyboard_events=True)
-	
-	def moveCursor(self,dx,dy):
-		if self.mode==PainterMode.NEUTRAL:
-			self.canvas.draw_point((self.cursor.x,self.cursor.y),size=20,color='white')
-			
-			self.window['CANVAS'].update()
-		
-		self.cursor.x += dx
-		self.cursor.y += dy
-		if self.mode==PainterMode.WRITE:
-			self.canvas.draw_point((self.cursor.x,self.cursor.y),5,color='black')
-		
-		elif self.mode==PainterMode.ERASE:
-			self.canvas.draw_point((self.cursor.x,self.cursor.y),10,color='white')
-		else:
-			self.canvas.draw_point((self.cursor.x,self.cursor.y),size=10,color='blue')
-	
-	def changeMode(self):
-		if self.mode==PainterMode.NEUTRAL:
-			self.canvas.draw_point((self.cursor.x,self.cursor.y),size=20,color='white')
-		
-		self.mode = (self.mode+1)%3
-		self.window['Mode'].update(MODE_TEXT[self.mode])
-	
-	def paint(self):
-		while True:
-			event,values = self.window.read()
-			
-			# self.window.bind('<Up>','-UP-')
-			# self.window.bind('<Down>','-DOWN-')
-			# self.window.bind('<Left>','-LEFT-')
-			# self.window.bind('<Right>','-RIGHT-')
-			# self.window.bind('<Return>','-SHIFT_MODE-')
-		
-			# dx = 0
-			# dy = 0
-			command = self.UART.getCommand()
-			print(command)
-			if command == 's':
-				self.changeMode()
-			else:
-				dx,dy=self.UART.getJoystickRead()
-			if event in (sg.WIN_CLOSED,'Exit'):
-				break
-			# if event=="-UP-":
-			# 	dy = 1
-			#
-			# elif event=='-DOWN-':
-			# 	dy = -1
-			#
-			# elif event=='-LEFT-':
-			# 	dx = -1
-			#
-			# elif event=='-RIGHT-':
-			# 	dx = 1
-			#
-			# elif event=='-SHIFT_MODE-':
-			# 	self.changeMode()
-			
-			self.moveCursor(dx,dy)
-			print(event)  # ------ Process menu choices ------ #
-		self.window.close()
+    def __init__(self, UART):
+        self.UART = UART
+        self.canvas = sg.Graph(CANVAS_RESOLUTION, (0, 0), CANVAS_RESOLUTION, key='CANVAS', background_color='white')
+        self.cursor = PaintCursor()
+        self.mode = PainterMode.NEUTRAL
+        layout = [[self.canvas], [sg.Text(MODE_TEXT[self.mode], key='Mode')]]
+        self.window = sg.Window("Painter", layout, return_keyboard_events=True)
+
+    def moveCursor(self, dx, dy):
+        if self.mode == PainterMode.NEUTRAL:
+            self.canvas.draw_point((self.cursor.x, self.cursor.y), size=20, color='white')
+
+            self.window['CANVAS'].update()
+
+        self.cursor.x += dx
+        self.cursor.y += dy
+        if self.mode == PainterMode.WRITE:
+            self.canvas.draw_point((self.cursor.x, self.cursor.y), 5, color='black')
+
+        elif self.mode == PainterMode.ERASE:
+            self.canvas.draw_point((self.cursor.x, self.cursor.y), 10, color='white')
+        else:
+            self.canvas.draw_point((self.cursor.x, self.cursor.y), size=10, color='blue')
+
+    def changeMode(self):
+        if self.mode == PainterMode.NEUTRAL:
+            self.canvas.draw_point((self.cursor.x, self.cursor.y), size=20, color='white')
+
+        self.mode = (self.mode + 1) % 3
+        self.window['Mode'].update(MODE_TEXT[self.mode])
+
+    def paint(self):
+        while True:
+            event, values = self.window.read()
+
+            # self.window.bind('<Up>','-UP-')
+            # self.window.bind('<Down>','-DOWN-')
+            # self.window.bind('<Left>','-LEFT-')
+            # self.window.bind('<Right>','-RIGHT-')
+            # self.window.bind('<Return>','-SHIFT_MODE-')
+
+            # dx = 0
+            # dy = 0
+            command = self.UART.getCommand()
+            print(command)
+            if command == 's':
+                self.changeMode()
+            else:
+                dx, dy = self.UART.getJoystickRead()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                break
+            # if event=="-UP-":
+            # 	dy = 1
+            #
+            # elif event=='-DOWN-':
+            # 	dy = -1
+            #
+            # elif event=='-LEFT-':
+            # 	dx = -1
+            #
+            # elif event=='-RIGHT-':
+            # 	dx = 1
+            #
+            # elif event=='-SHIFT_MODE-':
+            # 	self.changeMode()
+
+            self.moveCursor(dx, dy)
+            print(event)  # ------ Process menu choices ------ #
+        self.window.close()
+
 
 class UART:
-	def __init__(self):
-		self.channel = ser.Serial('COM3',baudrate=9600,bytesize=ser.EIGHTBITS,parity=ser.PARITY_NONE,stopbits=ser.STOPBITS_ONE,
-		               timeout=1)  # timeout of 1 sec so that the read and write operations are blocking,  # after the timeout the program continues
-		self.channel.reset_input_buffer()
-		self.channel.reset_output_buffer()
-		
-	def send(self, message):
-		enableTX = True
-		while self.channel.out_waiting>0 or enableTX:  # while the output buffer isn't empty
-			inChar = message
-			bytesChar = bytes(inChar,'ascii')
-			self.channel.write(bytesChar)
-			if self.channel.out_waiting==0 and ('x' or 'y' or 'c' in inChar):
-				enableTX = False
-	
-	def receive(self):
-		while self.channel.in_waiting>0:  # while the input buffer isn't empty
-			line = self.channel.read_until(terminator='\n')  # read  from the buffer until the terminator is received,
-			number = int.from_bytes(line,"big",signed=True)  # format is int.from_bytes(byte array, endian, signed/unsigned)
-			number_hex = hex(number)
-			
-			if self.channel.in_waiting==0:
-				return line # maybe return number_hex instead?
-			
-	def getCommand(self):
-		# self.send('c')
-		line = self.receive()
-		msg = bytes(line,'ascii')
-		return msg
-		# TODO - Add a way to translate command, maybe 'receive()' should output the bytes without translation
-	
-	def getData(self):
-		# self.send('d')
-		line = self.receive()
-		msg = int.from_bytes(line,"big",signed=True)  # format is int.from_bytes(byte array, endian, signed/unsigned)
-		return msg
-	
-	def getJoystickRead(self):
-		self.send('x')
-		x = self.receive()
-		self.send('y')
-		y = self.receive()
-		return x,y
+    def __init__(self):
+        self.channel = ser.Serial('COM3', baudrate=9600, bytesize=ser.EIGHTBITS, parity=ser.PARITY_NONE,
+                                  stopbits=ser.STOPBITS_ONE,
+                                  timeout=1)  # timeout of 1 sec so that the read and write operations are blocking,  # after the timeout the program continues
+        self.channel.reset_input_buffer()
+        self.channel.reset_output_buffer()
+
+    def send(self, message):
+        enableTX = True
+        while self.channel.out_waiting > 0 or enableTX:  # while the output buffer isn't empty
+            inChar = message
+            bytesChar = bytes(inChar, 'ascii')
+            self.channel.write(bytesChar)
+            if self.channel.out_waiting == 0 and ('x' or 'y' or 'c' in inChar):
+                enableTX = False
+
+    def receive(self):
+        while self.channel.in_waiting > 0:  # while the input buffer isn't empty
+            line = self.channel.read_until(terminator='\n')  # read  from the buffer until the terminator is received,
+            number = int.from_bytes(line, "big",
+                                    signed=True)  # format is int.from_bytes(byte array, endian, signed/unsigned)
+            number_hex = hex(number)
+
+            if self.channel.in_waiting == 0:
+                return line  # maybe return number_hex instead?
+
+    def getCommand(self):
+        # self.send('c')
+        line = self.receive()
+        while(line == None):
+            line = self.receive()
+        msg = chr(line[0])
+        print(msg)
+        return msg
+
+    # TODO - Add a way to translate command, maybe 'receive()' should output the bytes without translation
+
+    def getData(self):
+        # self.send('d')
+        line = self.receive()
+        msg = int.from_bytes(line, "big", signed=True)  # format is int.from_bytes(byte array, endian, signed/unsigned)
+        return msg
+
+    def getJoystickRead(self):
+        self.send('x')
+        x = self.receive()
+        self.send('y')
+        y = self.receive()
+        return x, y
+
 
 class MotorControlApp:
-	def __init__(self, UART):
-		self.UART = UART
-		layout = [sg.Text("Control Motor Using Joystick")]
-		self.window = sg.Window("Motor Control",layout,return_keyboard_events=True)
-	
-	def run(self):
-		while(True):
-			event,values = self.window.read()
-			if event in (sg.WIN_CLOSED,'Exit'):
-				self.UART.send(EXIT_MESSAGE)
-				break
+    def __init__(self, UART):
+        self.UART = UART
+        layout = [sg.Text("Control Motor Using Joystick")]
+        self.window = sg.Window("Motor Control", layout, return_keyboard_events=True)
+
+    def run(self):
+        while (True):
+            event, values = self.window.read()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                self.UART.send(EXIT_MESSAGE)
+                break
+
 
 class MotorCalibrationApp:
-	def __init__(self, UART):
-		self.UART = UART
-		
-		layout = [sg.Text("Press joystick button to start the motor"),
-		          sg.Text("Press again to stop when reaching 0 angle"),
-		          [sg.Text('FCS'), sg.Text(key='Nphi')]]
-		self.window = sg.Window("Motor Control",layout,return_keyboard_events=True)
-		
-	def run(self):
-		while(True):
-			fcs = self.UART.getData() # Full Circle Steps
-			nphi = self.UART.getData() # Nominal Angle
-			command = self.UART.getCommand()
-			if(command==EXIT_MESSAGE):
-				break
-	
+    def __init__(self, UART):
+        self.UART = UART
+
+        layout = [sg.Text("Press joystick button to start the motor"),
+                  sg.Text("Press again to stop when reaching 0 angle"),
+                  [sg.Text('FCS'), sg.Text(key='Nphi')]]
+        self.window = sg.Window("Motor Control", layout, return_keyboard_events=True)
+
+    def run(self):
+        while (True):
+            fcs = self.UART.getData()  # Full Circle Steps
+            nphi = self.UART.getData()  # Nominal Angle
+            command = self.UART.getCommand()
+            if (command == EXIT_MESSAGE):
+                break
+
+
 def main():
-	main_menu = MainMenu()
-	current_choice = 1
-	uart = UART()
-	while True:
-		event,values = main_menu.window.read()
-		
-		# main_menu.window.bind('<Up>','-UP-')
-		# main_menu.window.bind('<Down>','-DOWN-')
-		# main_menu.window.bind('<Return>','-CHOOSE-')
-		
-		if event in (sg.WIN_CLOSED,'Exit'):
-			break
-			
-		# dx,dy = uart.getJoystickRead()
-		command = uart.getCommand()
-		
-		if command==UP_MESSAGE:
-			if current_choice>1:
-				main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
-				current_choice = current_choice-1
-		# if event=="-UP-":
-		# 	if current_choice>1:
-		# 		main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
-		# 		current_choice = current_choice-1
-		elif command==DOWN_MESSAGE:
-			if current_choice<4:
-				main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
-				current_choice = current_choice+1
-		# elif event=='-DOWN-':
-		# 	if current_choice<4:
-		# 		main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
-		# 		current_choice = current_choice+1
-		elif command==BUTTON_PRESSED_MESSAGE:
-			if current_choice==1:
-				print("Motor Control")
-				mc = MotorControlApp()
-				mc.run()
-				
-			elif current_choice==2:
-				print("Painter")
-				painter = Painter()
-				painter.paint()
-			elif current_choice==3:
-				print("Motor Calibration")
-				mc = MotorCalibrationApp()
-				mc.run()
-			else:
-				print("Script Mode")
-		# elif event=='-CHOOSE-':
-		#
-		# 	if current_choice==2:
-		# 		painter = Painter()
-		# 		painter.paint()
-		
-		print(event,values)
-		# ------ Process menu choices ------ #
-		main_menu.window[current_choice].set_focus(force=True)
-		main_menu.window[current_choice].update(button_color=BUTTON_COLOR_CHOSEN)
-	
-	main_menu.window.close()
-	
+    main_menu = MainMenu()
+    current_choice = 1
+    uart = UART()
+    while True:
+        #event, values = main_menu.window.read()
+
+        # main_menu.window.bind('<Up>','-UP-')
+        # main_menu.window.bind('<Down>','-DOWN-')
+        # main_menu.window.bind('<Return>','-CHOOSE-')
+
+        #if event in (sg.WIN_CLOSED, 'Exit'):
+        #    break
+
+        # dx,dy = uart.getJoystickRead()
+        command = uart.getCommand()
+
+        if command == UP_MESSAGE:
+            if current_choice > 1:
+                main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
+                current_choice = current_choice - 1
+        # if event=="-UP-":
+        # 	if current_choice>1:
+        # 		main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
+        # 		current_choice = current_choice-1
+        elif command == DOWN_MESSAGE:
+            if current_choice < 4:
+                main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
+                current_choice = current_choice + 1
+        # elif event=='-DOWN-':
+        # 	if current_choice<4:
+        # 		main_menu.window[current_choice].update(button_color=BUTTON_COLOR_UNPRESSED)
+        # 		current_choice = current_choice+1
+        elif command == BUTTON_PRESSED_MESSAGE:
+            if current_choice == 1:
+                print("Motor Control")
+                mc = MotorControlApp()
+                mc.run()
+
+            elif current_choice == 2:
+                print("Painter")
+                painter = Painter()
+                painter.paint()
+            elif current_choice == 3:
+                print("Motor Calibration")
+                mc = MotorCalibrationApp()
+                mc.run()
+            else:
+                print("Script Mode")
+        # elif event=='-CHOOSE-':
+        #
+        # 	if current_choice==2:
+        # 		painter = Painter()
+        # 		painter.paint()
+
+        #print(event, values)
+        # ------ Process menu choices ------ #
+        main_menu.window[current_choice].set_focus(force=True)
+        main_menu.window[current_choice].update(button_color=BUTTON_COLOR_CHOSEN)
+
+    main_menu.window.close()
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
